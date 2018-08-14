73d7c659f11fdd24383ef0741f03d5cb700f6a03
Brian <brianpalowski00@gmail.com>
2018-08-14 11:52:02 -0700

    Add gitignore

--- src/main.js
+++ src/main.js
@@ -10,8 +10,8 @@ $(document).ready(function() {
     event.preventDefault();
     let userInput = $('#issue').val();
     let userInfo = $("#condition").val();
-  //  $('#issue').val("");
-   //Doctor.user(userInput,infoData, no);
+    //  $('#issue').val("");
+    //Doctor.user(userInput,infoData, no);
 
 
     function infoData(response){
@@ -23,54 +23,54 @@ $(document).ready(function() {
         for(let i= 0; i < response.data.length; i++){
           $('#showDetails').prepend(`<li> Name:  ${response.data[i].profile.first_name} ${response.data[i].profile.last_name} </li> <li>Address: ${response.data[i].practices[0].visit_address.city} ${response.data[i].practices[0].visit_address.state} </li><li>Phone: ${response.data[i].practices[0].phones[0].number}</li>
             <li>${response.data[i].profile.bio}</li> <li> Accepting New Customers: ${response.data[i].practices[0].accepts_new_patients == true} </li> <li><img src="${response.data[i].profile.image_url}"/></li>`);
-          console.log(response);
+            console.log(response);
+          }
         }
       }
-    }
 
-    function con(user){
-      if(user.data.length == 0){
-        $(".errors").append("There is currently nothing we can recommend to you");
-      }else if(!userInfo){
-        $(".errors").append();
-      }else{
-        for(let f= 0; f < user.data.length; f++){
-          $('#showDetails').prepend(`<li> Specialities: ${user.data[f].specialties[0].uid} </li><li>${user.data[f].specialties[0].description}</li><li>Name: ${user.data[f].profile.first_name} ${user.data[f].profile.last_name}</li><li>Address: ${user.data[f].practices[0].visit_address.city} ${user.data[f].practices[0].visit_address.state} </li><li>Phone: ${user.data[f].practices[0].phones[0].number}</li>`);
-          console.log(user);
+      function con(user){
+        if(user.data.length == 0){
+          $(".errors").append("There is currently nothing we can recommend to you");
+        }else if(!userInfo){
+          $(".errors").append();
+        }else{
+          for(let f= 0; f < user.data.length; f++){
+            $('#showDetails').prepend(`<li> Specialities: ${user.data[f].specialties[0].uid} </li><li>${user.data[f].specialties[0].description}</li><li>Name: ${user.data[f].profile.first_name} ${user.data[f].profile.last_name}</li><li><img src="${user.data[f].profile.image_url}"/><li>Address: ${user.data[f].practices[0].visit_address.city} ${user.data[f].practices[0].visit_address.state} </li><li>Phone: ${user.data[f].practices[0].phones[0].number}</li>`);
+            console.log(user);
+          }
         }
       }
-    }
 
-    // $('#showDetails').prepend(`<li>+Description: ${response.data[i].profile.first_name} </li>`);
-    // console.log(response);
-    //console.log(response)
-    // function toFind(find){
-    //   if(find.data.length == 0){
-    //     $("#errors").append("There is currently nothing we can recommend to you");
-    //   }else{
-    //     for(let b = 0; b < find.data.length; b++){
-    //       $('#showDetails').append(`<li>+Description: ${find.data[b].profile.first_name} </li>`);
-    //       console.log(find);
-    //     }
-    //
-    //   }
-    //   debugger;
-    //
-    // }
-    function no(){
+      // $('#showDetails').prepend(`<li>+Description: ${response.data[i].profile.first_name} </li>`);
+      // console.log(response);
+      //console.log(response)
+      // function toFind(find){
+      //   if(find.data.length == 0){
+      //     $("#errors").append("There is currently nothing we can recommend to you");
+      //   }else{
+      //     for(let b = 0; b < find.data.length; b++){
+      //       $('#showDetails').append(`<li>+Description: ${find.data[b].profile.first_name} </li>`);
+      //       console.log(find);
+      //     }
+      //
+      //   }
+      //   debugger;
+      //
+      // }
+      function no(){
 
-  }
-  // function why(){
-  //   if(!userInfo){
-  //   $(".errors").append("why");
-  // }
-  // }
-
+      }
+      // function why(){
+      //   if(!userInfo){
+      //   $(".errors").append("why");
+      // }
+      // }
 
 
-$('#issue').val("");
-$('#condition').val("");
-Doctor.user(userInput,infoData, no);
-Doctor.mix(userInfo,con,no );
+
+      $('#issue').val("");
+      $('#condition').val("");
+      Doctor.user(userInput,infoData, no);
+      Doctor.mix(userInfo,con,no );
+    });
   });
-});
--- src/starter.js
+++ src/starter.js
@@ -2,7 +2,7 @@ export class Doctor {
 
 
   static user(name,toFind,no){
-    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=or&user_location=45.5122%2C%20122.6587&skip=0&limit=5&user_key=25f0d8d86aa79dd348ea8c186ec09b57`)
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${name}&location=or&user_location=45.5122%2C%20122.6587&skip=0&limit=5&user_key=${process.env.exports.apiKey}`)
     .then(function(sort){
       toFind(sort);
     }).catch(function(){
@@ -11,7 +11,7 @@ export class Doctor {
   }
 
   static mix(query,input,why){
-    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${query}&location=or&user_location=45.5122%2C%20122.6587&skip=0&limit=5&user_key=d05b49d7c8d433f57cdf6ae2770bbcfb`)
+    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${query}&location=or&skip=0&limit=5&user_key=${process.env.exports.apiKey}`)
 
     .then(function(show){
       input(show);
